349b39222cb28b7617430f2d2bc5b51ca2a2bfe3
Dmitrii Maliukov <bcomedian@gmail.com>
2022-06-26 14:11:41 -0400

    Added async using WebRequest methods

--- PlayWithAsync/MainWindow.xaml.cs
+++ PlayWithAsync/MainWindow.xaml.cs
@@ -16,12 +16,15 @@ namespace PlayWithAsync
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<byte[]> _apmImgData = new ();
         private static readonly List<string> _picUrls = new ()
         {
             "https://media.gettyimages.com/photos/entrance-to-uncompahgre-national-forest-colorado-circa-1962-picture-id168642438?s=2048x2048",
             "https://media.gettyimages.com/photos/jackson-lake-snake-river-and-mount-moran-in-the-grand-teton-national-picture-id492841003?s=2048x2048",
             "https://media.gettyimages.com/photos/35mm-film-photo-shows-an-automobile-making-its-way-down-an-empty-dirt-picture-id1315492753?s=2048x2048",
-            "https://media.gettyimages.com/photos/white-sands-national-monument-new-mexico-usa-circa-1960-picture-id532273617?s=2048x2048"
+            "https://media.gettyimages.com/photos/white-sands-national-monument-new-mexico-usa-circa-1960-picture-id532273617?s=2048x2048",
+            "https://media.gettyimages.com/photos/the-colorado-river-wraps-around-horseshoe-bend-in-the-in-glen-canyon-picture-id647219426?s=2048x2048",
+            "https://media.gettyimages.com/photos/canyon-de-chelly-navajo-by-edward-s-curtis-depicting-navajo-riders-picture-id530194319?s=2048x2048"
         };
 
         public MainWindow()
@@ -90,12 +93,45 @@ namespace PlayWithAsync
             }
             RenderImages(dataFromServer);
         }
-        
+
         private void BtnLoadData_WebRequest_Sync_OnClick(object sender, RoutedEventArgs e)
-        { }
+        {
+            var imgData = new List<byte[]>(); 
+            foreach (var url in _picUrls)
+            {
+                var request = (HttpWebRequest) WebRequest.Create(new Uri(url));
+                var response = (HttpWebResponse) request.GetResponse();
+                using var binaryReader = new BinaryReader(response.GetResponseStream());
+                imgData.Add(binaryReader.ReadBytes((int) response.ContentLength));
+            }
+            RenderImages(imgData);
+        }
 
         private void BtnLoadData_WebRequest_Async_APM_OnClick(object sender, RoutedEventArgs e)
-        { }
+        {
+            foreach (var picUrl in _picUrls)
+            {
+                var req = (HttpWebRequest) WebRequest.Create(new Uri(picUrl));
+                req.BeginGetResponse(ProcessEndResponse, req);
+            }
+
+            while (_apmImgData.Count != _picUrls.Count)
+            { }
+            
+            RenderImages(_apmImgData);
+        }
+
+        private void ProcessEndResponse(IAsyncResult ar)
+        {
+            var req = (HttpWebRequest) ar.AsyncState;
+            var response = (HttpWebResponse) req.EndGetResponse(ar);
+            using var binaryReader = new BinaryReader(response.GetResponseStream());
+            var data = binaryReader.ReadBytes((int)response.ContentLength);
+            lock (_apmImgData)
+            {
+                _apmImgData.Add(data);
+            }
+        }
 
         private async void BtnLoadData_WebRequest_Async_TAP_OnClick(object sender, RoutedEventArgs e)
         { }
